1128d3dcac67f5497f4dd85ddf287f2ea345b9df
root <root@ip-172-31-28-81.us-east-2.compute.internal>
2018-04-19 15:01:00 +0000

    finally inserted pictures into html

--- recipes/server.rb
+++ recipes/server.rb
@@ -8,15 +8,31 @@ service 'httpd' do
     action [:enable, :start]
 end
 
+remote_file '/var/www/html/splash.png' do
+     source 'https://www.pngarts.com/files/1/Water-Splash-PNG-Download-Image.png'
+end
 
 
-cookbook_file '/var/www/html/index.html' do
-   source 'index.html'
+template '/var/www/html/index.html' do
+     source 'index.html.erb'
 end
 
 
 
 
+
+
+
+
+# for static file
+#cookbook_file '/var/www/html/index.html' do
+#   source 'index.html'
+#end
+
+
+
+
+#for dynamic file
 #template  '/var/www/html/index.html' do
 #        source 'index.html.erb'
 #end 
