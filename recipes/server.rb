a09a345ee8d58f7e67beeccbb8ab3d2b883779f7
root <root@ip-172-31-28-81.us-east-2.compute.internal>
2018-04-19 13:34:37 +0000

    cookbook_file

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,19 +1,24 @@
 package 'httpd' do
-
    action :install 
-
 end
 
 
 
 service 'httpd' do
-
     action [:enable, :start]
-
 end
 
-template  '/var/www/html/index.html' do
-        source 'index.html.erb'
-end 
+
+
+cookbook_file '/var/www/html/index.html' do
+   source 'index.html'
+end
+
+
+
+
+#template  '/var/www/html/index.html' do
+#        source 'index.html.erb'
+#end 
 
  
