5e6ce176c325ff8aa20a35024aae50a3f65ec13f
root <root@ip-172-31-28-81.us-east-2.compute.internal>
2018-04-19 13:25:22 +0000

    updated erb

--- recipes/server.rb
+++ recipes/server.rb
@@ -12,15 +12,8 @@ service 'httpd' do
 
 end
 
-file  '/var/www/html/index.html' do
-
-     content "<h1>Details of this machine </h1>
-     <h2>HOSTNAME: #{node['hostname']}</h2>
-     <h3>IPADDRESS: #{node['ipaddress']}</h3>
-     <h3>CPU:#{node['cpu']['0']['mhz']}</h3>
-     <h4>MEMORY:#{node['memory']['total']}</h4>
-"
-
+template  '/var/www/html/index.html' do
+        source 'index.html.erb'
 end 
 
  
